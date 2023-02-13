c37104322d36b6c5ddbfa681ab9e75fb3cf753e1
adimunawar31 <munawar.adi31@gmail.com>
2023-02-13 07:05:02 +0700

    change name app

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,11 +5,11 @@ const Banner = () => {
 		<div className='my-4 hidden md:flex h-52 justify-center'>
 			<div className='grid max-w-5xl grid-cols-6 rounded-xl bg-blue-700'>
 				<div className='col-span-4'>
-					<h2 className='mt-8 ml-10 text-3xl font-bold text-white'>Selamat Datang di PRESENCE APP</h2>
+					<h2 className='mt-8 ml-10 text-3xl font-bold text-white'>Selamat Datang di Aplikasi Presensi</h2>
 
 					<p className='my-5 ml-10 text-sm font-light text-white'>
-						PRESENCE APP merupakan terobosan baru pada sistem absensi pegawai. Selain bisa menekan pengeluaran
-						operasional, PRESENCE APP juga mempermudah dalam melakukan perhitungan kehadiran karena data sudah
+						Aplikasi Presensi merupakan terobosan baru pada sistem absensi pegawai. Selain bisa menekan pengeluaran
+						operasional, Aplikasi Presensi juga mempermudah dalam melakukan perhitungan kehadiran karena data sudah
 						terintegrasi dengan aplikasi mobilenya
 					</p>
 				</div>
